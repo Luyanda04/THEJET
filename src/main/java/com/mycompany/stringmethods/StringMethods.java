f8c0a5ae0918b4fb97160a747435df648f80f095
RC_Student_lab <RC_Student_lab@10.0.0.145>
2024-09-10 12:19:08 +0000

    First Commit

---
+++ src/main/java/com/mycompany/stringmethods/StringMethods.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stringmethods;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class StringMethods {
+
+    public static void main(String[] args) {
+        //charAt() => returns  a character
+        String name = " Hello to ";
+        String lname = " HelloTwo ";
+        char results = name.charAt(0);
+        System.out.println(results);
+        System.out.println(name.charAt(3));
+        
+        //CompareTo() => returns integer
+        System.out.println(name.compareTo(lname));
+        
+        //concat() => returns a string
+         System.out.println(name.concat(lname));
+         
+         //contains()
+         String email = "jet@gmail.com";
+         System.out.println(email.contains("@"));
+         
+         //EndsWith => returns boolean
+         System.out.println(email.endsWith(".com"));
+        
+         //equals => returns boolean
+         System.out.println(email.equals("jet@gmail.com"));
+          System.out.println(email.equalsIgnoreCase("jet@gmail.com"));
+          
+          //indexOf() => Returns an integer
+          System.out.println(email.indexOf("."));
+          
+          //IsEmpty()
+           System.out.println(email.isEmpty());
+           
+           //length() => Returns integer
+             System.out.println(email.length());
+}
+}
